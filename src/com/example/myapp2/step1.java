39afdb50a2a60e460b3a26c082d30b9bda7c78fe
Eva Dögg Steingrímsdóttir <evadoggsteingrimsdottir@od-dw418.rhi.hi.is>
2014-10-11 16:00:54 +0000

    Lagaði villur

--- src/com/example/myapp2/step1.java
+++ src/com/example/myapp2/step1.java
@@ -11,7 +11,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
